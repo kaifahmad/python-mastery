ebf8b429d768c4d8b05731c969bf7cd0b3c718fe
Mohd Amaan <amaanshavez@gmail.com>
2023-05-06 11:13:44 +0530

    Basic Jupiter Setup

--- scikit-learn/03-classifier.py
+++ scikit-learn/03-classifier.py
@@ -1,6 +1,16 @@
 from sklearn import datasets
+from sklearn.neighbors import KNeighborsClassifier 
+
 iris = datasets.load_iris()
 
-features = iris.keys()
-_DESC = iris.DESCR
-print(_DESC)
+all_keys = iris.keys()
+features = iris.data[:-1]
+target_labels = iris.target[:-1]
+print(all_keys)
+
+# Training the Classifier
+k_near_classifier = KNeighborsClassifier()
+k_near_classifier.fit(features,target_labels)
+
+# Cross-checking prediction with Actual label
+print(f"{k_near_classifier.predict(features[-1:])}, Actual : {target_labels[-1:]}")
